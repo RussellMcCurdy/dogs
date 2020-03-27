77ffa24480dae05e88a068a63e62298232970bef
Russell McCurdy <Russell.Alan.McCurdy@gmail.com>
2020-03-27 16:11:56 -0500

    number 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dogPic = [];
 function getDogImage() {
     fetch('https://dog.ceo/api/breeds/image/random/'+ dogNumber)
       .then(response => response.json())
-      .then(responseJson => letsLog(responseJson));
+      .then(responseJson => displayDogs(responseJson));
   };
 
 function dogInput() {
@@ -30,4 +30,17 @@ function letsLog(dogPics)
     for (i=0; i < dogPics.message.length; i++) {
         console.log(dogPics.message[i]);
     };
+};
+
+function displayDogs(dogPics) {
+    let emptyHtml=``;
+    $('.dogAnnounce').html(emptyHtml);
+    $('.results').removeClass('hidden');
+    for (i=0; i < dogPics.message.length; i++) {
+        console.log(dogPics.message[i]);
+        $('.dogAnnounce').append(
+            `<div class = "dogImage">
+            <img src="${dogPics.message[i]}" class="results-img">
+            </div>`
+        )};
 };
